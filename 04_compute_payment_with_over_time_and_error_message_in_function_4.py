b949050fdb07ef8e3d1214a323258d4908b3800b
Laboni Laboni <iamlaboniraz@gmail.com>
2019-01-05 21:19:49 +0600

    04_compute_payment_with_over_time_and_error_message_in_function_4

---
+++ 04_compute_payment_with_over_time_and_error_message_in_function_4.py
@@ -0,0 +1,27 @@
+import sys
+def computepay(hours, rate):
+        try:
+             hours = int(input('Enter Hours: '))
+             rate = float(input('Enter Rate:'))
+             #x=int(hours)
+             #y=float(rate)
+             if hours>40:
+                 rate_calculate = 1.5 * hours/(40)
+             else:
+                rate_calculate = 0
+             final_rate = rate * 1.5 * rate_calculate
+             pay = hours * rate + final_rate
+             return pay
+
+        except ValueError:
+            print("Error, please enter numeric input")
+        
+hours = int()
+rate = float()
+pay = computepay(hours, rate)
+if pay:
+    print('Pay: {:.2f}'.format(pay))
+
+
+
+    
