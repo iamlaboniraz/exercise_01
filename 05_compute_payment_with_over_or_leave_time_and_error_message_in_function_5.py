8937fab819e4267cf4dd72eedd650d1d03f5bed9
Laboni Laboni <iamlaboniraz@gmail.com>
2019-01-05 21:20:44 +0600

    05_compute_payment_with_over_or_leave_time_and_error_message_in_function_5

---
+++ 05_compute_payment_with_over_or_leave_time_and_error_message_in_function_5.py
@@ -0,0 +1,32 @@
+import sys
+def computepay(hours, rate):
+        try:
+             hours = int(input('Enter Hours: '))
+             rate = float(input('Enter Rate:'))
+             #x=int(hours)
+             #y=float(rate)
+             if hours>40:
+                 rate_calculate = 1.5 * hours/(40)
+                 final_rate = rate * 1.5 * rate_calculate
+                 
+             elif hours<40:
+                 rate_calculate = 0.5 - hours/(40)
+                 final_rate = rate * rate_calculate
+             else:
+                rate_calculate = 0
+                final_rate = rate * rate_calculate
+             pay = hours * rate + final_rate
+             return pay
+
+        except ValueError:
+            print("Error, please enter numeric input")
+        
+hours = int()
+rate = float()
+pay = computepay(hours, rate)
+if pay:
+    print('Pay: {:.2f}'.format(pay))
+
+
+
+    
